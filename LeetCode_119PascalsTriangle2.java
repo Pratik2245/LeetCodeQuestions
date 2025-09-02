975075cfa014f8714d7c2f1eff2df551bc4609e8
Pratik2245 <paithankarpratik3@gmail.com>
2025-09-02 11:22:17 +0530

    LeetCode_119PascalsTriangle2 done

---
+++ LeetCode_119PascalsTriangle2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_119PascalsTriangle2 {
+    public static void main(String[] args) {
+      int row=30;
+      getRow(row);
+    }
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list=new ArrayList<>();
+        long res=1;
+        list.add((int)res);
+        for(int col=1;col<=rowIndex;col++){
+            res=res*(rowIndex+1-col)/col;
+            list.add((int)res);
+        }
+        return list;
+    }
+}
